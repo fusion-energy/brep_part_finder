ded0367609f0702d0338ac5bbf83e45378cbd056
Jonathan <mail@jshimwell.com>
2021-12-28 23:25:33 +0000

    added volume and faces check

--- brep_part_finder/core.py
+++ brep_part_finder/core.py
@@ -1,13 +1,37 @@
 from cadquery import *
 from cadquery.occ_impl.shapes import Shape
+import numpy as np
+
 
 def import_brep(filename):
     my_brep = Shape.importBrep(filename)
     return my_brep
 
-def find_part(shape_object, volume=None):
-    for part in shape_object.Solids():
-        print('volume', part.Volume())
+def find_part(
+    shape_object,
+    volume=None,
+    volume_atol=1e-8,
+    faces=None
+):
+    matching_volume_ids = []
+    for counter, part in enumerate(shape_object.Solids(), 1):
+        print()
+        print(counter)
+        print(' volume', part.Volume())
+        print(' faces', len(part.Faces()))
+
+    for counter, part in enumerate(shape_object.Solids(), 1):
+        if volume:
+            if np.isclose(part.Volume(), volume, atol=volume_atol):
+                print('matching volumes')
+                matching_volume_ids.append(counter)
+
+    for counter, part in enumerate(shape_object.Solids(), 1):
+        if faces:
+            if faces == len(part.Faces()):
+                print('matching faces')
+                matching_volume_ids.append(counter)
         # print('CenterOfBoundBox', part.CenterOfBoundBox)
         # print('faces', len(part.Faces()))
         # print('area', len(part.Area()))
+    return list(set(matching_volume_ids))
--- examples/paramak_example.py
+++ examples/paramak_example.py
@@ -1,6 +1,7 @@
 import brep_part_finder as bpf
 import paramak
 
+
 my_reactor = paramak.BallReactor()
 my_reactor.export_brep('my_reactor.brep')
 print(my_reactor.volume())
@@ -9,7 +10,9 @@ my_reactor_brep_shapes = bpf.import_brep('my_reactor.brep')
 
 part_id = bpf.find_part(
     shape_object = my_reactor_brep_shapes,
-    volume=10
+    volume=220932912,
+    volume_atol=1e-6,
+    faces=10
 )
 
 print(part_id)
