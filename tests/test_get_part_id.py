218f64f5d8faac7a8d7a487c57cb4209a7e26516
Jonathan Shimwell <mail@jshimwell.com>
2022-01-17 15:05:02 +0000

    fixing tests

--- tests/test_get_part_id.py
+++ tests/test_get_part_id.py
@@ -12,8 +12,7 @@ class TestShape(unittest.TestCase):
         """"""
 
         part_id = bpf.get_part_id(
-            # brep_part_properties = my_reactor_brep_shapes,
-            brep_part_properties = self.my_reactor_brep_shapes,
+            brep_part_properties = self.brep_part_properties,
             volume=95467959.26023674,
             volume_atol=1e-6,
         )
@@ -24,8 +23,7 @@ class TestShape(unittest.TestCase):
         """"""
 
         part_id = bpf.get_part_id(
-            # brep_part_properties = my_reactor_brep_shapes,
-            brep_part_properties = self.my_reactor_brep_shapes,
+            brep_part_properties = self.brep_part_properties,
             center=(-0.006133773543690803, 7.867805031206607e-10, 7.70315160988196e-12),
             center_atol=1e-6,
         )
@@ -36,8 +34,7 @@ class TestShape(unittest.TestCase):
         """"""
 
         part_id = bpf.get_part_id(
-            # brep_part_properties = my_reactor_brep_shapes,
-            brep_part_properties = self.my_reactor_brep_shapes,
+            brep_part_properties = self.brep_part_properties,
             bounding_box=[[-570.5554844464615, -570.5554844464615, -453.27123145033755], [570.5554844464615, 570.5554844464615, 453.27123145033755]],
             bounding_box_atol=1e-6,
         )
