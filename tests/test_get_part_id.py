255fd72ddf3fdfe124c8c2cc273e3d31c290870d
shimwell <shimwell@users.noreply.github.com>
2022-01-17 15:00:13 +0000

    [skip ci] Apply formatting changes

--- tests/test_get_part_id.py
+++ tests/test_get_part_id.py
@@ -1,4 +1,3 @@
-
 import unittest
 import brep_part_finder as bpf
 
@@ -6,14 +5,14 @@ import brep_part_finder as bpf
 class TestShape(unittest.TestCase):
     def setUp(self):
 
-        self.brep_part_properties = bpf.get_brep_part_properties('ball_reactor.brep')
+        self.brep_part_properties = bpf.get_brep_part_properties("ball_reactor.brep")
 
     def test_finding_part_id_with_volume(self):
         """"""
 
         part_id = bpf.get_part_id(
             # brep_part_properties = my_reactor_brep_shapes,
-            brep_part_properties = self.my_reactor_brep_shapes,
+            brep_part_properties=self.my_reactor_brep_shapes,
             volume=95467959.26023674,
             volume_atol=1e-6,
         )
@@ -25,7 +24,7 @@ class TestShape(unittest.TestCase):
 
         part_id = bpf.get_part_id(
             # brep_part_properties = my_reactor_brep_shapes,
-            brep_part_properties = self.my_reactor_brep_shapes,
+            brep_part_properties=self.my_reactor_brep_shapes,
             center=(-0.006133773543690803, 7.867805031206607e-10, 7.70315160988196e-12),
             center_atol=1e-6,
         )
@@ -37,8 +36,11 @@ class TestShape(unittest.TestCase):
 
         part_id = bpf.get_part_id(
             # brep_part_properties = my_reactor_brep_shapes,
-            brep_part_properties = self.my_reactor_brep_shapes,
-            bounding_box=[[-570.5554844464615, -570.5554844464615, -453.27123145033755], [570.5554844464615, 570.5554844464615, 453.27123145033755]],
+            brep_part_properties=self.my_reactor_brep_shapes,
+            bounding_box=[
+                [-570.5554844464615, -570.5554844464615, -453.27123145033755],
+                [570.5554844464615, 570.5554844464615, 453.27123145033755],
+            ],
             bounding_box_atol=1e-6,
         )
 
