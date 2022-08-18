733cb562be2c22afb2fe72d8f17b25801792d1ec
shimwell <mail@jshimwell.com>
2022-08-18 21:38:59 +0100

    added version tag

--- brep_part_finder/__init__.py
+++ brep_part_finder/__init__.py
@@ -1,3 +1,17 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except (ModuleNotFoundError, ImportError):
+    from importlib_metadata import version, PackageNotFoundError
+try:
+    __version__ = version("brep_part_finder")
+except PackageNotFoundError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = ["__version__"]
+
+
 from .core import (
     get_brep_part_properties,
     get_part_id,
