e67f7f188174da3d106b7583f649ffe6911d066d
Jonathan Shimwell <mail@jshimwell.com>
2022-09-20 16:11:04 +0100

    changed api to use the same args and keys

--- brep_part_finder/core.py
+++ brep_part_finder/core.py
@@ -1,3 +1,4 @@
+from turtle import shape
 import warnings
 from collections.abc import Iterable
 from typing import Tuple, Union
@@ -8,7 +9,46 @@ from cadquery.occ_impl.shapes import Shape
 import cadquery as cq
 
 
-def get_brep_part_properties_from_shape(shapes: Shape):
+def get_part_properties_from_shape(shape: Shape) -> dict:
+    """Accepts a cadquery solid object and returns the unique
+    identify details of the solid
+
+    Args:
+        filename: the filename of the brep file
+    """
+
+    part_details = {}
+    part_details["center_x"] = shape.Center().x
+    part_details["center_y"] = shape.Center().y
+    part_details["center_z"] = shape.Center().z
+
+    part_details["volume"] = shape.Volume()
+
+    part_details["bounding_box_xmin"] = shape.BoundingBox().xmin
+    part_details["bounding_box_ymin"] = shape.BoundingBox().ymin
+    part_details["bounding_box_zmin"] = shape.BoundingBox().zmin
+    part_details["bounding_box_xmax"] = shape.BoundingBox().xmax
+    part_details["bounding_box_ymax"] = shape.BoundingBox().ymax
+    part_details["bounding_box_zmax"] = shape.BoundingBox().zmax
+
+    return part_details
+
+
+def convert_shape_to_iterable_of_shapes(shapes):
+
+    if isinstance(shapes, cq.occ_impl.shapes.Compound):
+        # brep route
+        iterable_solids = shapes.Solids()
+    elif isinstance(shapes, cq.Workplane):
+        # workplane route
+        iterable_solids = shapes.val().Solids()
+    else:
+        iterable_solids = shapes.Solids()
+
+    return iterable_solids
+
+
+def get_part_properties_from_shapes(shapes: Iterable) -> dict:
     """Accepts a cadquery.occ_impl.shapes object and returns the unique
     identify details of each Solid
 
@@ -16,33 +56,20 @@ def get_brep_part_properties_from_shape(shapes: Shape):
         filename: the filename of the brep file
     """
 
-    if isinstance(shapes, cq.occ_impl.shapes.Compound):
-        iterable_solids = shapes.Solids()
+    if not isinstance(shapes, Iterable):
+        iterable_of_shapes = convert_shape_to_iterable_of_shapes(shapes)
     else:
-        iterable_solids = shapes.val().Solids()
+        iterable_of_shapes = shapes
 
     all_part_details = {}
-    for counter, part in enumerate(iterable_solids, 1):
-        part_details = {}
-        part_details["Center.x"] = part.Center().x
-        part_details["Center.y"] = part.Center().y
-        part_details["Center.z"] = part.Center().z
-
-        part_details["Volume"] = part.Volume()
-
-        part_details["BoundingBox.xmin"] = part.BoundingBox().xmin
-        part_details["BoundingBox.ymin"] = part.BoundingBox().ymin
-        part_details["BoundingBox.zmin"] = part.BoundingBox().zmin
-        part_details["BoundingBox.xmax"] = part.BoundingBox().xmax
-        part_details["BoundingBox.ymax"] = part.BoundingBox().ymax
-        part_details["BoundingBox.zmax"] = part.BoundingBox().zmax
-
+    for counter, part in enumerate(iterable_of_shapes, 1):
+        part_details = get_part_properties_from_shape(part)
         all_part_details[counter] = part_details
 
     return all_part_details
 
 
-def get_brep_part_properties(filename: Union[str, PathLike]):
+def get_part_properties_from_file(filename: Union[str, PathLike]):
     """Imports a Brep CAD file and returns the unique identify details of each Solid
 
     Args:
@@ -51,16 +78,23 @@ def get_brep_part_properties(filename: Union[str, PathLike]):
 
     shapes = Shape.importBrep(filename)
 
-    my_brep_part_details = get_brep_part_properties_from_shape(shapes)
+    my_brep_part_details = get_part_properties_from_shapes(shapes)
 
     return my_brep_part_details
 
 
 def get_part_id(
     brep_part_properties: dict,
+    center_x: float = None,
+    center_y: float = None,
+    center_z: float = None,
     volume: float = None,
-    center: Tuple[float, float, float] = None,
-    bounding_box: Tuple[Tuple[float, float, float], Tuple[float, float, float]] = None,
+    bounding_box_xmin: float = None,
+    bounding_box_ymin: float = None,
+    bounding_box_zmin: float = None,
+    bounding_box_xmax: float = None,
+    bounding_box_ymax: float = None,
+    bounding_box_zmax: float = None,
     volume_atol: float = 1e-6,
     center_atol: float = 1e-6,
     bounding_box_atol: float = 1e-6,
@@ -84,72 +118,22 @@ def get_part_id(
 
     part_ids_matching = {}
 
-    if center:
-        part_ids_matching_centers = []
-        for key, value in brep_part_properties.items():
-            if (
-                np.isclose(value["Center.x"], center[0], atol=center_atol)
-                and np.isclose(value["Center.y"], center[1], atol=center_atol)
-                and np.isclose(value["Center.z"], center[2], atol=center_atol)
-            ):
-                part_ids_matching_centers.append(key)
-        if len(part_ids_matching_centers) == 0:
-            warnings.warn(
-                "No parts matching the specified center +/- tolerances were found"
-            )
-        else:
-            part_ids_matching["center"] = part_ids_matching_centers
-
-    if volume:
-        part_ids_matching_volume = []
-        for key, value in brep_part_properties.items():
-            if np.isclose(value["Volume"], volume, atol=volume_atol):
-                part_ids_matching_volume.append(key)
-        if len(part_ids_matching_volume) == 0:
-            warnings.warn(
-                "No parts matching the specified volume +/- tolerances were found"
-            )
-        else:
-            part_ids_matching["volume"] = part_ids_matching_volume
-
-    if bounding_box:
-        part_ids_matching_bounding_box = []
-        for key, value in brep_part_properties.items():
-            part_bb = (
-                value["BoundingBox.xmin"],
-                value["BoundingBox.ymin"],
-                value["BoundingBox.zmin"],
-            ), (
-                value["BoundingBox.xmax"],
-                value["BoundingBox.ymax"],
-                value["BoundingBox.zmax"],
-            )
-            if (
-                np.isclose(part_bb[0][0], bounding_box[0][0], atol=bounding_box_atol)
-                and np.isclose(
-                    part_bb[0][1], bounding_box[0][1], atol=bounding_box_atol
+    properties = [center_x, center_y, center_z, volume, bounding_box_xmin, bounding_box_ymin, bounding_box_zmin, bounding_box_xmax, bounding_box_ymax, bounding_box_zmax]
+    properties_names = ["center_x", "center_y", "center_z", "volume", "bounding_box_xmin", "bounding_box_ymin", "bounding_box_zmin", "bounding_box_xmax", "bounding_box_ymax", "bounding_box_zmax"]
+    tolerances = [center_atol, center_atol, center_atol, volume_atol, bounding_box_atol, bounding_box_atol, bounding_box_atol, bounding_box_atol, bounding_box_atol, bounding_box_atol]
+    
+    for property, names, tolerance in zip(properties, properties_names, tolerances):
+        if property is not None:
+            part_ids_matching_property = []
+            for key, value in brep_part_properties.items():
+                if np.isclose(value[names], property, atol=tolerance):
+                    part_ids_matching_property.append(key)
+            if len(part_ids_matching_property) == 0:
+                warnings.warn(
+                    f"No parts matching the specified {names} +/- tolerances were found"
                 )
-                and np.isclose(
-                    part_bb[0][2], bounding_box[0][2], atol=bounding_box_atol
-                )
-                and np.isclose(
-                    part_bb[1][0], bounding_box[1][0], atol=bounding_box_atol
-                )
-                and np.isclose(
-                    part_bb[1][1], bounding_box[1][1], atol=bounding_box_atol
-                )
-                and np.isclose(
-                    part_bb[1][2], bounding_box[1][2], atol=bounding_box_atol
-                )
-            ):
-                # print('match',key,bounding_box,part_bb)
-                part_ids_matching_bounding_box.append(key)
-        if len(part_ids_matching_bounding_box) == 0:
-            warnings.warn("No parts matching the specified bounding boxes were found")
-        else:
-            part_ids_matching["bounding_box"] = part_ids_matching_bounding_box
-
-    # print("volume numbers matching search criteria", part_ids_matching)
+            else:
+                part_ids_matching[names] = part_ids_matching_property
 
     lists_of_matching_parts_separate = list(part_ids_matching.values())
 
@@ -157,11 +141,20 @@ def get_part_id(
         warnings.warn("No single part found that matches all criteria")
         print("search criteria are:")
         print(" volume", volume)
-        print(" center", center)
-        print(" bounding_box", bounding_box)
-        print(" volume_atol", volume_atol)
-        print(" center_atol", center_atol)
-        print(" bounding_box_atol", bounding_box_atol)
+        print(" center_x", center_x)
+        print(" center_y", center_y)
+        print(" center_z", center_z)
+        print(" bounding_box_xmin", bounding_box_xmin)
+        print(" bounding_box_ymin", bounding_box_ymin)
+        print(" bounding_box_zmin", bounding_box_zmin)
+        print(" bounding_box_xmax", bounding_box_xmax)
+        print(" bounding_box_ymax", bounding_box_ymax)
+        print(" bounding_box_zmax", bounding_box_zmax)
+        print(' with tolerances')
+        print("  volume_atol", volume_atol)
+        print("  center_atol", center_atol)
+        print("  bounding_box_atol", bounding_box_atol)
+        raise ValueError('No matching part found')
 
     lists_of_matching_parts = list(
         set.intersection(*map(set, lists_of_matching_parts_separate))
@@ -196,17 +189,16 @@ def get_part_ids(
 
 
 def get_dict_of_part_ids(
-    brep_part_properties,
-    shape_properties: list,
+    brep_part_properties: dict,
+    shape_properties: dict,
     volume_atol: float = 1e-6,
     center_atol: float = 1e-6,
     bounding_box_atol: float = 1e-6,
 ):
+    """finds the brep id that matches the shape ids and returns a linking"""
     key_and_part_id = {}
 
-    for entry in shape_properties:
-        key = entry[0]
-        value = entry[1]
+    for key, value in shape_properties:
 
         if isinstance(value, dict):
             # check if value is a list of dictionaries or a dictionary
@@ -221,22 +213,8 @@ def get_dict_of_part_ids(
                 raise ValueError(f"multiple matching volumes were found for {key}")
             # todo check that key is not already in use
             key_and_part_id[matching_part_id[0]] = key
-        elif isinstance(value, Iterable):
-            # assumed to be list
-            for entry in value:
-                # check if value is a list of dictionaries or a dictionary
-                matching_part_id = get_part_id(
-                    brep_part_properties=brep_part_properties,
-                    volume_atol=volume_atol,
-                    center_atol=center_atol,
-                    bounding_box_atol=bounding_box_atol,
-                    **entry,
-                )
-                if len(matching_part_id) > 1:
-                    raise ValueError(f"multiple matching volumes were found for {key}")
-                # todo check that key is not already in use
-                key_and_part_id[matching_part_id[0]] = key
+
         else:
-            msg = "dictionary values must be either a dictionary or a list of dictionaries"
+            msg = "shape_properties must be a dictionary of dictionaries"
             raise ValueError(msg)
     return key_and_part_id
