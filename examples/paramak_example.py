b37819fdc3d7a6f9977f6277f6977aed104efe84
Jonathan Shimwell <mail@jshimwell.com>
2022-09-21 11:40:31 +0100

    updated examples api usage

--- examples/paramak_example.py
+++ examples/paramak_example.py
@@ -2,17 +2,17 @@ import os
 
 import paramak
 
-my_reactor = paramak.BallReactor()
+my_reactor = paramak.BallReactor(rotation_angle=90)
 
 # order of parts gets mixed when saved to brep file
 my_reactor.export_dagmc_h5m(
     filename="dagmc.h5m",
-    min_mesh_size=1,
-    max_mesh_size=3,
+    min_mesh_size=5,
+    max_mesh_size=10,
     exclude=[
         "plasma"
     ],  # does not mesh the plasma as not many neutron interactions occur
 )
 
-# converting for viewing geometry in Paraview / Visit
+# converting for viewing geometry in Paraview / Visit using the Moab tool mbconvert
 # os.system("mbconvert dagmc.h5m dagm.vtk")
